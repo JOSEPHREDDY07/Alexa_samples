c11e101f0200f3128d22280e870cef90fa6388d6
Joseph R yeruva <josephreddy07@gmail.com>
2017-08-24 16:32:06 -0400

    Update facts-en-US.js

--- data/facts-en-US.js
+++ data/facts-en-US.js
@@ -1,21 +1,19 @@
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Benjamin Moore Paintsis an American company that produces paint. It is owned by Berkshire Hathaway. Founded in 1883.",
+    "The Moore Brothers founded the company in Brooklyn, New York and now It is based in Montvale, New Jersey",
+    "Benjamin Moore Paints Ranked 1 in Customer Satisfaction by J.D. Power and Associates in 2011 & 2012.",
+    "Regal,Aura,Natura,MoorGard,Ben,Arborcoat,Ultra Spec,Insl-X,Corotech are major products of Benjamin moore paints.",
+    "Benjamin Moore produces the highest-quality paints and finishes in the industry, and deliver them to independent retail locations",
+    "Today, Benjamin Moore is the 128-year-old company is one of the largest paint makers in North America, with 7 plants, 22 distribution facilities, and roughly 4,000 independent retailers.",
+    "Berkshire Hathaway, a company led by billionaire Warren Buffett, acquired Benjamin Moore",
+    "Today, Benjamin Moore & Co, a Berkshire Hathaway company, is a high-performing, innovative manufacturer",
+    "BENJAMIN MOORE INTRODUCES ULTRA SPEC SCUFF-X AS THE INDUSTRY'S FIRST SCUFF-RESISTANT PAINT",
+    "Benjamin Moore Industry Leader Introduces 75 New Colors and First-Ever Soft Touch Matte Finish.",
+    "BENJAMIN MOORE REVEALS “SHADOW” AS ITS COLOR OF THE YEAR 2017."
 ];
 
-var SKILL_NAME = "American Space Facts";
+var SKILL_NAME = "American BMC Facts";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say tell me a Benjamin moore fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
